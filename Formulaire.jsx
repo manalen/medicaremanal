435993cf82cb2b0f1ba3f7d2f1e3f9744368555a
manalen <61988178+manalen@users.noreply.github.com>
2020-04-06 20:44:18 +0100

    Update Formulaire.jsx

--- Formulaire.jsx
+++ Formulaire.jsx
@@ -8,24 +8,14 @@ class Formulaire extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            service: '',
+            consultation: '',
             nommed: '',
             nomp: '',
             dateconsult: ''
-            //consultation: {},
-            //message: null
         }
-        //this.refreshConsult=this.refreshConsult.bind(this)
     }
 
-    /*componentDidMount() {
-        this.refreshConsult();
-    }*/
 
-    /*refreshConsult() {
-        fetch( 'http://localhost:8087/medecin/creerconsultation'), {
-            method: 'POST',
-    }*/
     onChange = (e) => {
         const state = this.state
         state[e.target.name] = e.target.value;
@@ -35,9 +25,9 @@ class Formulaire extends Component {
     onSubmit = (e) => {
         e.preventDefault();
 
-        const {service,nommed,nomp,dateconsult} = this.state;
+        const {consultation,nommed,nomp,dateconsult} = this.state;
 
-        axios.post('/medecin/creerconsultation', {service,nommed,nomp,dateconsult})
+        axios.post('/medecin/creerconsultation', {consultation,nommed,nomp,dateconsult})
             .then((result) => {
                 this.props.history.push("/")
             });
@@ -53,21 +43,21 @@ class Formulaire extends Component {
                         <div class="row">
                             <div class="form-group">
                                 <label htmlFor="lbl_service" id="lidc">Consultation</label>
-                                <input type="text" class="form-control" id="service" required="required" onChange={this.onChange}/>
+                                <input type="text" class="form-control" id="consultation" name="consult"  required="required" value={consultation} onChange={this.onChange}/>
                             </div>
                         </div>
                         <div class="form-group">
                             <label htmlFor="lbl_nommed">Medecin</label>
-                            <input type="text" class="form-control" id="nommed" required="required" onChange={this.onChange}/>
+                            <input type="text" class="form-control" id="nommed" name="medecin" required="required" value={nommed} onChange={this.onChange}/>
                         </div>
                         <div class="form-group">
                             <label htmlFor="lbl_nomp">Patient</label>
-                            <input type="text" class="form-control" id="nomp" required="required" onChange={this.onChange} />
+                            <input type="text" class="form-control" id="nomp" name="patient" required="required" value={nomp} onChange={this.onChange} />
                         </div>
                         <div class="row">
                             <div class="form-group">
                                 <label htmlFor="lbl_dateconsultation" id="dateconsult">Date de consultation</label>
-                                <input type="date" class="form-control" id="dateconsultation" required="required" onChange={this.onChange} />
+                                <input type="date" class="form-control" id="dateconsultation" name="date" required="required" value={dateconsult} onChange={this.onChange} />
                             </div>
                         </div>
                     </form>
